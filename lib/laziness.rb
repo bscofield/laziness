33bdf2602663fabd6c3f40c037c56108a248b4a7
ben <ben@e959a6d6-1924-0410-92b3-a6fa492f4c66>
2008-03-20 13:30:07 +0000

    customize assertion for thrown exception
    git-svn-id: http://svn.extendviget.com/lab/laziness/trunk@577 e959a6d6-1924-0410-92b3-a6fa492f4c66

--- lib/laziness.rb
+++ lib/laziness.rb
@@ -12,7 +12,7 @@ module Laziness
         @template.send!(:assign_variables_from_controller)
 
         contents  = @template.render_file(template_path_for_local_rescue(exception), false)
-        test      = Laziness.generate_test(request.method.to_s, params, session.instance_variable_get(:@data), cookies)
+        test      = Laziness.generate_test(exception, request.method.to_s, params, session.instance_variable_get(:@data), cookies)
         @template.instance_variable_set("@contents", contents + test)
 
         response.content_type = Mime::HTML
@@ -21,24 +21,21 @@ module Laziness
     end
   end
 
-  def self.generate_test(method, params, session, cookies)
-    session    ||= {}
-    cookies    ||= {}
-    controller   = params.delete(:controller)
-    action       = params.delete(:action)
-    params       = params || {}
-    flash        = session.delete(:flash) || {}
+  def self.generate_test(exception, method, params = {}, session = {}, cookies = {})
+    controller = params.delete(:controller)
+    action     = params.delete(:action)
+    params     = params || {}
+    flash      = session.delete('flash') || {}
 "
 <h2 style='margin-top:30px;'>Laziness Test</h2>
 
 <pre>
-def test_#{method}_#{controller}_#{action}_should_not_raise_exception
-  assert_nothing_raised do
+def test_#{method}_#{controller}_#{action}_should_not_raise_#{exception.class.name.downcase}_exception
+  assert_nothing_raised(#{exception.class.name}) do
     #{method} :#{action}, #{params.inspect}, #{session.inspect}, #{flash.inspect}, #{cookies.inspect}
   end
 end
 </pre>
 "
   end
-
 end
