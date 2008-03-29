89749b3b147ef86dd0c87c1c8555e3c4719fa6d7
Ben Scofield <git@turrean.com>
2008-03-29 01:14:46 -0400

    add TODO comments

--- lib/exceptions/action_controller/unknown_view.rb
+++ lib/exceptions/action_controller/unknown_view.rb
@@ -8,19 +8,22 @@ module Laziness
           action     = params.delete(:action)
 
           if File.exists?("#{RAILS_ROOT}/spec/spec_helper.rb")
+# TODO: validate that this spec works as expected
             return "
 describe \"Lazy #{controller_class}\" do
   it \"should have the action #{action}\" do
     #{controller_class}.action_methods.should include('#{action}')
   end
 end
-"
+" 
           else
             return "
 def test_#{controller_class.downcase}_should_have_the_action_#{action}
   assert #{controller_class}.action_methods.include?('#{action}')
 end
 "
+
+# TODO: add extra tests around routing and recognition?
           end
         end
       end
